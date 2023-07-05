061cd7b3fef84ece35853610f3752e76ae937834
Jeremy Toussaint <jt151077@gmail.com>
2023-07-05 14:11:18 +0200

    Update osdu.py

--- osdu.py
+++ osdu.py
@@ -2,15 +2,13 @@ import requests
 import json
 import subprocess
 
-headers={}
-
 def setHeaders(bearer):
-  headers = {
+  return headers = {
     "data-partition-id": "odesprod",
     "Authorization": bearer
   }
 
-def get_groups_success_scenario():
+def get_groups_success_scenario(headers):
   response = requests.get('https://preship.gcp.gnrg-osdu.projects.epam.com/api/entitlements/v2/groups', headers=headers)
   return json.dumps(response.json())
 
@@ -25,5 +23,5 @@ def authorize(client_id, client_secret, output):
   output.clear()
   response = subprocess.check_output("curl -d client_id={} -d client_secret={} -d grant_type=authorization_code -d redirect_uri=https%3A%2F%2Foauth.pstmn.io%2Fv1%2Fbrowser-callback -d code={} https://accounts.google.com/o/oauth2/token".format(client_id, client_secret, auth_code), shell=True).decode().split("\n")[1:6]
   access_token, expires_in, refresh_token, scope, token_type = formatx(0, "access_token", 0, 0, response), formatx(1, "expires_in", 1, 0, response), formatx(2, "refresh_token", 0, 0, response), formatx(3, "scope", 0, 0, response), formatx(4, "token_type", 0, 1, response)
-  setHeaders(("Bearer {}").format((access_token.split("access_token: "))[1]))
-  return "Authentication successful"
+  return setHeaders(("Bearer {}").format((access_token.split("access_token: "))[1]))
+  
